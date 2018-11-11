c1b77fa054290a3f8bc4004766301f4add743467
Alex Kersten <alex@kersten.email>
2018-11-11 13:55:21 -0600

    include jquery types

--- src/ts/app/app.ts
+++ src/ts/app/app.ts
@@ -2,9 +2,16 @@
  * Interactive portion of the garden - logic and rendering for the canvas.
  */
 
+import * as $ from "jquery";
+
 class app
 {
-    constructor() {
+    private canvas: JQuery<HTMLCanvasElement>;
 
+    constructor() {
+        this.canvas = $("#cutegarden-canvas");
     }
 }
+
+
+new app();
