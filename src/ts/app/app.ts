265297825467706f082c8eaceeea7a6d4c878668
Alex Kersten <alex@kersten.email>
2018-11-11 20:52:46 -0600

    add skeleton for other classes in app

--- src/ts/app/app.ts
+++ src/ts/app/app.ts
@@ -7,13 +7,42 @@ class app {
     private $canvasContainer: JQuery<HTMLElement>;
 
     private ctx: CanvasRenderingContext2D;
-    private cw: number;
-    private ch: number;
+    private _cw: number;
+    private _ch: number;
 
-    private fps: number;            // Frames-per-second
-    private framesDrawn: number;    // How many frames have been drawn in the last second
+    private _fps: number;            // Frames-per-second
+    private _framesDrawn: number;    // How many frames have been drawn in the last second
 
     private config: AppConfig;
+
+    private _cam: Camera;
+
+
+    //#region Getters / setters
+
+    public get cw(): number {
+        return this._cw;
+    }
+
+    public get ch(): number {
+        return this._ch;
+    }
+
+
+    public get fps(): number {
+        return this._fps;
+    }
+
+    public get framesDrawn(): number {
+        return this._framesDrawn;
+    }
+
+
+    get cam(): Camera {
+        return this._cam;
+    }
+
+    //#endregion
 
     constructor() {
         this.init();
@@ -27,12 +56,13 @@ class app {
         this.initElements();
         this.initListeners();
         this.initCanvas();
+        this.initCamera();
         this.initTimers();
     }
 
     private initVars(): void {
-        this.fps = 0;
-        this.framesDrawn = 0;
+        this._fps = 0;
+        this._framesDrawn = 0;
 
         this.config = new AppConfig();
     }
@@ -43,7 +73,9 @@ class app {
     }
 
     private initListeners(): void {
-        window.addEventListener("resize", this.onResize);
+        $(window).on("resize", () => {
+            this.onResize();
+        });
     }
 
     private initCanvas(): void {
@@ -51,8 +83,15 @@ class app {
         this.canvasResizeToContainer();
     }
 
+
+    private initCamera(): void {
+        this._cam = new Camera(-this.cw / 2, -this.ch / 2);
+    }
+
     private initTimers(): void {
-        window.setInterval(this.updateFPS, 1000);
+        window.setInterval(() => {
+            this.updateFPS();
+        }, 1000);
     }
 
 
@@ -73,8 +112,8 @@ class app {
     //#region Timers
 
     private updateFPS(): void {
-        this.fps = this.framesDrawn;
-        this.framesDrawn = 0;
+        this._fps = this.framesDrawn;
+        this._framesDrawn = 0;
     }
 
     //#endregion
@@ -85,22 +124,19 @@ class app {
      * @summary Kick off the canvas's rendering - the canvas will automatically be re-rendered.
      */
     private animate(): void {
-        window.requestAnimationFrame((timestamp: DOMHighResTimeStamp) => {
-            this.animate_impl(timestamp);
+        window.requestAnimationFrame(() => {
+            this.animate_impl();
         });
     }
 
     /**
      * @summary Automatically call canvasRedraw when the next animation frame is ready.
      */
-    private animate_impl(timestamp: DOMHighResTimeStamp): void {
-       // console.log("" + timestamp);
-        if (timestamp < timestamp) {
-            return;
-        }
-        window.requestAnimationFrame((timestamp: DOMHighResTimeStamp) => {
-            this.animate_impl(timestamp);
+    private animate_impl(): void {
+        window.requestAnimationFrame(() => {
+            this.animate_impl();
         });
+
         this.canvasRedraw();
     }
 
@@ -108,11 +144,11 @@ class app {
      * @summary Resize the canvas to fill its containing element.
      */
     public canvasResizeToContainer() {
-        this.cw = this.$canvasContainer.innerWidth();
-        this.ch = this.$canvasContainer.innerHeight();
+        this._cw = this.$canvasContainer.width();
+        this._ch = this.$canvasContainer.height();
 
-        this.$canvas.width(this.cw);
-        this.$canvas.height(this.ch);
+        (this.$canvas.get(0) as HTMLCanvasElement).width = this.cw;
+        (this.$canvas.get(0) as HTMLCanvasElement).height = this.ch;
     }
 
     /**
@@ -121,14 +157,8 @@ class app {
     public canvasRedraw(): void {
         this.ctx.clearRect(0, 0, this.cw, this.ch);
 
-
-        if (this.config.showFPS) {
-            this.ctx.fillStyle = "#00EE00";
-            this.ctx.font = AppConstants.FONT_SANS_SERIF;
-            this.ctx.fillText("FPS: " + this.fps, 16, 16);
-        }
-
-        this.framesDrawn++;
+        Renderer.drawDebugInfo(this, this.config, this.ctx);
+        this._framesDrawn++;
     }
 
 
@@ -136,10 +166,12 @@ class app {
 }
 
 class AppConfig {
-    public showFPS: boolean;
+    public debug_showFPS: boolean;
+    public debug_showCanvasBoundingBox: boolean;
 
     constructor() {
-        this.showFPS = true;
+        this.debug_showFPS = true;
+        this.debug_showCanvasBoundingBox = true;
     }
 
 }
@@ -149,6 +181,105 @@ class AppConstants {
     public static FONT_SANS_SERIF = "Yantramanav";
     public static FONT_SERIF = "Roboto Slab";
 
+    public static COLOR_CUTE_WHITE = "#e4e3e5";
+}
+
+class Renderer {
+    public static drawDebugInfo(a: app, config: AppConfig, ctx: CanvasRenderingContext2D) {
+        if (config.debug_showCanvasBoundingBox) {
+            ctx.strokeStyle = "#EE0000";
+            ctx.strokeRect(0, 0, a.cw, a.ch);
+            ctx.strokeStyle = "#00EE00";
+            ctx.strokeRect(1, 1, a.cw - 2, a.ch - 2);
+            ctx.strokeStyle = "#0000EE";
+            ctx.strokeRect(2, 2, a.cw - 4, a.ch - 4);
+        }
+
+        if (config.debug_showFPS) {
+            ctx.fillStyle = "#00EE00";
+            ctx.font = "20px " + AppConstants.FONT_SANS_SERIF;
+            ctx.fillText("FPS: " + a.fps, 16, 16);
+        }
+    }
+}
+
+class Camera {
+    private _offsetX: number;
+    private _offsetY: number;
+
+    get offsetX(): number {
+        return this._offsetX;
+    }
+
+    set offsetX(_: number) {
+        this._offsetX = _;
+    }
+
+    get offsetY(): number {
+        return this._offsetY;
+    }
+
+    set offsetY(_: number) {
+        this._offsetY = _;
+    }
+
+
+    constructor(offsetX: number = 0, offsetY: number = 0) {
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+    }
+}
+
+
+class HexTile {
+
+    private readonly TILE_RADIUS = 8;
+
+    private _posX: number;
+    private _poxY: number;
+
+    private _color: string;
+
+    //#region Getters / setters
+
+    public get posX(): number {
+        return this._posX;
+    }
+
+    public set posX(_: number) {
+        this._posX = _;
+    }
+
+    public get posY(): number {
+        return this._poxY;
+    }
+
+    public set posY(_: number) {
+        this._poxY = _;
+    }
+
+    public get color(): string {
+        return this._color;
+    }
+
+    public set color(_: string) {
+        this._color = _;
+    }
+
+    constructor(x: number = 0, y: number = 0, color: string = AppConstants.COLOR_CUTE_WHITE) {
+        this.posX = x;
+        this.posY = y;
+        this.color = color;
+    }
+
+    public render(cam: Camera, ctx: CanvasRenderingContext2D): void {
+        ctx.beginPath();
+
+
+
+        ctx.closePath();
+        ctx.stroke();
+    }
 }
 
 new app();
